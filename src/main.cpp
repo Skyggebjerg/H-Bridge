1c39736abfb40d4126e6c192a3a89ec08e29237a
Skyggebjerg <ole@skyggebjerg.dk>
2024-02-23 19:18:43 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -19,23 +19,25 @@ void setup() {
 }
 
 void loop() {
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 500; i++){
         driver.setDriverDirection(1); // Set peristaltic pump in forward to take out BR content
-        driver.setDriverSpeed8Bits(255); //Run pump in full speed
-        Serial.print("Forward pump for 5 secs #: ");
+        driver.setDriverSpeed8Bits(128); //Run pump in half speed
+        Serial.print("Forward pump for 30 msecs #: ");
         Serial.println(i);
-        delay(5000);
+        delay(30);
         driver.setDriverSpeed8Bits(0);  //Stop pump 
-        Serial.println("Wait for 30 secs");     
-        delay(30000);         
+        Serial.println("Wait for 4.7 secs");     
+        delay(4700);         
         }
         
+        for(int i = 0; i < 16; i++){
         driver.setDriverDirection(2); // Set peristaltic pump in reverse to rinse 
-        driver.setDriverSpeed8Bits(255);
-        Serial.println("Reverse pump for 5 secs #: ");
-        delay(5000);
+        driver.setDriverSpeed8Bits(128);
+        Serial.println("Reverse pump for 30 msecs #: ");
+        delay(30);
         driver.setDriverSpeed8Bits(0);             
-        delay(1000);         
+        delay(200); 
+        }        
     //Serial.println("VIN IN: " + String(driver.getAnalogInput(_12bit) / 4095.0f * 3.3f / 0.09f));
     //Serial.println("current: " + String(driver.getMotorCurrent())); 
 }
